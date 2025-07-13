4c33187c0062d179d5fc57b421eba9fcf0302696
Abaasa Albert <altechalbert01@gmail.com>
2025-07-13 14:25:47 +0300

    fixed all

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:altech@001@localhost:5432/luco"
 
 DB_URI = "postgresql://lucosms_fr6s_user:XW8sLMPg8BPAKJmKbyymPokMLJZx2oia@dpg-d1c5d8euk2gs73acoa8g-a.oregon-postgres.render.com/lucosms_fr6s"
-# DB_URI = "postgresql://postgres:xecmKsNUQBtnuitVSwuKuzaVkGlnFmcT@shinkansen.proxy.rlwy.net:46354/railway"
+
 engine = create_engine(DB_URI, connect_args={"sslmode": "require"})
 
 
